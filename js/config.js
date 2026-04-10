b43490b4d4b17efa2393e68b73cc95f72e24713b
yhonatanback <yhonatanback@galaxypay.app>
2026-04-09 21:18:03 -0500

    fix: configuración de la ruta del archivo config

--- js/config.js
+++ js/config.js
@@ -1,7 +1,6 @@
 const CONFIG = {
   API_BASE_URL: "api/config.php",
-};
-
-const API_ENDPOINTS = {
-  SEARCH_PROPERTIES: CONFIG.API_BASE_URL,
+  API_ENDPOINTS: {
+    SEARCH_PROPERTIES: "api/config.php",
+  },
 };
